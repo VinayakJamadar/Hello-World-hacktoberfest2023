fa9c514f20706807295b7a37174ae43df9c3ceb9
Vinayak Jamadar <96077344+VinayakJamadar@users.noreply.github.com>
2023-10-01 13:13:00 +0530

    Update TypeScript.ts

--- TypeScript.ts
+++ TypeScript.ts
@@ -1,1 +1,9 @@
-console.log 'Hello World'
+let message: string = 'Hello World';
+console.log(message);
+
+// To compile typescript code we can run the following command on the command line.
+// tsc TypeScript.ts
+// This command will generate a javascript file with name TypeScript.js
+
+// Run the javascript file using the following command on command line:
+// node TypeScript.js
